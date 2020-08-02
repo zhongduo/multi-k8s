5523ec53764d0f7129afd2dc2a4f8f1b3fe99f68
Jimmy Lin <zhongduo@google.com>
2020-08-02 12:42:09 -0400

    add travis

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t zhongduo/multi-client:latest -t zhongduo/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t zhongduo/multi-server:latest -t zhongduo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t zhongduo/multi-worker:latest -t zhongduo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push zhongduo/multi-client:latest
+docker push zhongduo/multi-server:latest
+docker push zhongduo/mutli-worker:latest
+docker push zhongduo/multi-client:$SHA
+docker push zhongduo/multi-server:$SHA
+docker push zhongduo/mutli-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=zhongduo/multi-server:$SHA
+kubectl set image deployments/client-deployment client=zhongduo/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=zhongduo/multi-worker:$SHA
