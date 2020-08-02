bf968b9aba10126dd8a8785b8b0b87936bc0eb94
Jimmy Lin <zhongduo@google.com>
2020-08-02 12:53:34 -0400

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ docker build -t zhongduo/multi-server:latest -t zhongduo/multi-server:$SHA -f ./
 docker build -t zhongduo/multi-worker:latest -t zhongduo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push zhongduo/multi-client:latest
 docker push zhongduo/multi-server:latest
-docker push zhongduo/mutli-worker:latest
+docker push zhongduo/multi-worker:latest
 docker push zhongduo/multi-client:$SHA
 docker push zhongduo/multi-server:$SHA
-docker push zhongduo/mutli-worker:$SHA
+docker push zhongduo/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=zhongduo/multi-server:$SHA
 kubectl set image deployments/client-deployment client=zhongduo/multi-client:$SHA
